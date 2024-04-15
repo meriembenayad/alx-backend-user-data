ceb3fd3dd7f4d83bd76137e98d793d9f17c1b69f
meriembenayad <meriem.benayad@gmail.com>
2024-04-15 19:33:15 +0000

    Task 8: Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """ 6. Basic Auth """
 from api.v1.auth.auth import Auth
+# 8. Base64 decode
+import base64
+
 
 class BasicAuth(Auth):
     """ BasicAuth class for """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
             Return Base64 part of the Authorization header
             for a Basic Authorization
@@ -20,3 +24,23 @@ class BasicAuth(Auth):
 
         # Return the part of authorization_header after `Basic `
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+            Return decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # Decode the Base64 string
+            decode_bytes = base64.b64decode(base64_authorization_header)
+            # Convert the bytes to utf-8 string
+            return decode_bytes.decode('utf-8')
+        except Exception:
+            return None
