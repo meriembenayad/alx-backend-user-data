3e5d52c7ad7dd94b73f5e78c5020f5a060ab11d8
meriembenayad <meriem.benayad@gmail.com>
2024-04-17 23:49:06 +0100

    Task 5: Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,7 +45,7 @@ def before_request_func():
     if auth.require_auth(request.path, excluded_paths):
         auth_header = auth.authorization_header(request)
         user = auth.current_user(request)
-        if auth_header is None and auth.session_cookie(request):
+        if auth_header is None and auth.session_cookie(request) is None:
             abort(401)
 
         if user is None:
