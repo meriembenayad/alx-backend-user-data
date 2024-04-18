96e50671be75b1f8c9ab9447fa4e95778bdd6197
meriembenayad <meriem.benayad@gmail.com>
2024-04-18 14:29:41 +0100

    Task 7: Fix checkers 8 - 10 - 12

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -24,7 +24,7 @@ def login() -> str:
         return jsonify({"error": "password missing"}), 400
 
     user = User.search({'email': email})
-    if len(user) == 0:
+    if len(user) <= 0:
         return jsonify({"error": "no user found for this email"}), 404
 
     user = user[0]
@@ -32,7 +32,7 @@ def login() -> str:
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    session_id = auth.create_session(user_id)
+    session_id = auth.create_session(user.id)
     cookie_name = getenv('SESSION_NAME')
     response = jsonify(user.to_json())
     response.set_cookie(cookie_name, session_id)
