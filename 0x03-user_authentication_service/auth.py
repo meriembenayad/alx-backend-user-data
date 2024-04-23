a772adb62e953c281a578ed21684ac61bbf56647
meriembenayad <meriem.benayad@gmail.com>
2024-04-23 15:03:28 +0100

    Task 4: Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+""" 4. Hash password """
+from user import User
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    4. Hash password
+    Convert password to bytes (Hash it)
+    """
+    # Generate a random salt
+    salt = gensalt()
+
+    # Use salt to hash password
+    hashed_password = hashpw(password.encode(), salt)
+
+    return hashed_password
