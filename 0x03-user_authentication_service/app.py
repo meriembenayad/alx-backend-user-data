8ab171db4f6fbda181122fcdaf14cdfa5a79813f
meriembenayad <meriem.benayad@gmail.com>
2024-04-23 15:56:52 +0100

    Task 6: Basic Flask App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" 6. Basic Flask app """
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """ 6. method return JSON payload """
+    return jsonify({'message': 'Bienvenue'})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
