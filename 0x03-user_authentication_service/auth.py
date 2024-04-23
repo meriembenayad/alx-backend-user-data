04655b806b316be4a7c9abafd77ed4a9432bcc0c
meriembenayad <meriem.benayad@gmail.com>
2024-04-23 17:04:45 +0100

    Task 10: Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,13 @@ class Auth:
             return checked_password
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ 10. Get session ID """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return 'None'
