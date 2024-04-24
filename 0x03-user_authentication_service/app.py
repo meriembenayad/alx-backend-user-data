98efb3101db6f7b6492824135c642aaa2da2f08e
meriembenayad <meriem.benayad@gmail.com>
2024-04-24 01:38:57 +0100

    Task 14: Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,11 +56,12 @@ def logout() -> None:
     """ 14. Log out """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        redirect('/')
-    else:
+
+    if user is None:
         abort(403)
+
+    AUTH.destroy_session(user.id)
+    redirect('/')
 
 
 if __name__ == "__main__":
