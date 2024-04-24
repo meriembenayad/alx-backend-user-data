8b15ded815fad0b3b0d4eb5e3aafcaa5bb154350
meriembenayad <meriem.benayad@gmail.com>
2024-04-24 01:26:11 +0100

    Task 13: Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,12 +86,19 @@ class Auth:
     def get_user_from_session_id(self,
                                  session_id: str
                                  ) -> Union[TypeVar('User'), None]:
-        """ 11. Find user by session ID """
+        """ 12. Find user by session ID """
         if session_id is None:
             return None
 
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ 13. Destroy session """
+        try:
+            self._db.update_user(user_id=user_id, session_id=None)
         except NoResultFound:
             return None
