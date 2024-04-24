2613ccbe601634c3d8ec1b38d6a6464d70317f43
meriembenayad <meriem.benayad@gmail.com>
2024-04-24 20:11:05 +0100

    Task 19: Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,9 +97,10 @@ def update_password() -> str:
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
